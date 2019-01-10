c87d045f471fdcf0aed43df2027c856b9fb6d44b
丁蓉 <dingrong@shinho.net.cn>
2019-01-10 10:52:37 +0800

    新增vue.config.js配置项

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
+const path = require('path')
 module.exports = {
 	// baseUrl 默认 根路径 '/'
 	baseUrl: process.env.NODE_ENV === 'production'?'/production-sub-path':'/',
@@ -15,5 +16,50 @@ module.exports = {
 				target: '<other_url>'
 			}
 		}
+	},
+	// build将构建好的文件输出到哪里
+	outputDir: 'dist',
+
+	// build时放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录
+	assetsDir: '',
+
+	// 指定生成的 index.html 的输出路径 (相对于 outputDir)。也可以是一个绝对路径。
+	indexPath: 'index.html',
+
+	// 是否在保存的时候使用 eslint-loader 检查
+	lintOnSave: true,
+
+	// 默认在生成的静态资源文件名中包含hash以控制缓存
+	filenameHashing: true,
+
+	// 使用带有浏览器内编译器的完整构建版本
+	// compiler: false,
+
+	// bable-loader 默认会跳过 node_modules 依赖
+	// 通过这个选项可以显示转译一个依赖
+	transpileDependencies: [/* string or regex*/],
+
+	// 是否为生产环境构建生成 source map
+	productionSourceMap: false,
+
+	// 调整内部的 webpack 配置
+	chainWebpack: () => { },
+	configureWebpack: () => { },
+
+	// css相关
+	css: {
+		// 为预处理器的loader 传递自定义选项，比如传递给 sass-loader 时,使用`{sass: { ... }}`
+		loaderOptions: {
+			stylus: {
+				import: [
+					path.join(__dirname, './src/styles/common.styl'), // 一次性引入全局样式
+				],
+				paths: [
+					path.join(__dirname, './src/styles'),
+					path.join(__dirname, './')
+				]
+			}
+		}
 	}
+
 }
